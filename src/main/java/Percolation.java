1b10c06899a44365bbdb78197096c03e16f8785f
kenny <konadip@wal-mart.com>
2017-05-19 19:03:39 -0500

    modified code

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,5 +1,11 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+
+/**
+ * 
+ * @author Kehinde Onadipe
+ *
+ */
 public class Percolation {
 	WeightedQuickUnionUF uf;
 	int columns;
@@ -22,24 +28,27 @@ public class Percolation {
 	public void open(int row, int col) { // open site (row, col) if it is not								// open already
 	    if (!isOpen(row, col)) {
 	    	int id = xyToID(row, col);
-	    	//to open a site, set the value to 0;
-	    	if (id == columns) {
-	    		arr[id] = imaginarySite1;
-	    	} else {
-	    		arr[id] = 0;
+	    	//to open a site, set the value to n * n;
+	    	uf.union(imaginarySite1, id);
+	    	if (id < columns) {
+	    		arr[id] = imaginarySite2;
 	    	}
-	    	if (col + 1 < columns && isOpen(row, col + 1)) {
-	    		uf.union(id, id + 1);
-	  
+	    	
+	    	if ((col + 1 <= columns) || isOpen(row, col + 1)) {
+	    		int idRight = xyToID(row, col + 1);
+	    		uf.union(id, idRight);
 	    	}
-	    	if (col - 1 >= 0 && isOpen(row, col - 1)) {
-	    		uf.union(id, id - 1);
+	    	if (col - 1 > 0  && isOpen(row, col - 1)) {
+	    		int idLeft = xyToID(row, col - 1);
+	    		uf.union(id, idLeft);
 	    	}
-	    	if (row + 1 < columns && isOpen(row + 1, col)) {
-	    		uf.union(id, id + columns);
+	    	if (row + 1 <= columns && isOpen(row + 1, col)) {
+	    		int idUp = xyToID(row + 1, col);
+	    		uf.union(idUp, id);
 	    	}
-	    	if (row - 1 >= 0 && isOpen(row - 1, col)) {
-	    		uf.union(id, id - columns);
+	    	if (row - 1 > 0 && isOpen(row - 1, col)) {
+	    		int idDown = xyToID(row - 1, col);
+	    		uf.union(idDown, id);
 	    	}
 	    }
 	   
@@ -47,26 +56,19 @@ public class Percolation {
 
 	public boolean isOpen(int row, int col) { // is site (row, col) open?
 		int id = xyToID(row, col);
-		return arr[id] == 0;
+		int root = uf.find(id);
+		return root == imaginarySite1;
 	}
 
 	public boolean isFull(int row, int col) { // is site (row, col) full?
 		int id = xyToID(row, col);
-		boolean status = false;
-		for (int i = 0;i < columns; i++) {
-			int topIds = xyToID(1, i);
-			if (uf.connected(id, topIds)) {
-				return true;
-			}
-		}
-		
-		return false;
+		return uf.connected(id, imaginarySite2);
 	}
 
 	public int numberOfOpenSites() { // number of open sites
 		int count = 0; 
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == i+1) {
+			if (arr[i] != i+1) {
 				count++;
 			}
 		}
@@ -84,27 +86,24 @@ public class Percolation {
 	}
 	
 	private int xyToID(int row, int col) {
-		if ( row == 1) {
-			return col;
-		} else {
-			int id = row + columns * (col-1);
-			return id;
-		}
+		return (row - 1) * columns + (col - 1);
 	}
 	
 	public static void main(String[] args) { // test client (optional)
-		Percolation p = new Percolation(5);
+		Percolation p = new Percolation(1);
 		p.open(1, 1);
-		p.open(1, 2);
-		p.open(2, 2);
+
 		int id1 = p.xyToID(1, 1);
-		int id2 = p.xyToID(1, 3);
+		System.out.println(id1);
+		
 		
 		System.out.println(p.uf.find(id1));
-		System.out.println(p.uf.find(id2));
-		System.out.println(p.uf.connected(id1, id2));
-		System.out.println(p.isFull(2, 3));
+
+		//System.out.println(p.uf.connected(id1, id1));
 		
+		System.out.println(p.isFull(1, 1));
+		
+		System.out.println(p.numberOfOpenSites());
 		System.out.println(p.percolates());
 	
 	}
