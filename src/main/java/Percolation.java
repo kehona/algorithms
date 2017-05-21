388d8414395494d2eb38cfe08bb42fcde03ae134
kenny <konadip@wal-mart.com>
2017-05-21 15:31:57 -0500

    fixed bugs

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,111 +1,112 @@
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-
 /**
- * 
  * @author Kehinde Onadipe
- * This is the solution to the percolation problem in Princeton Algorithms class.
  * 
- * The Percolation Datastructure uses the WeightedQuickUnion datastructure to maintain states
- * of connected sites.
- * 
- * In Percolation, we using another grid to  maintain sites
- * 
- *
+ *         This is the solution to the percolation problem in Princeton
+ *         Algorithms class. The Percolation Datastructure uses the
+ *         WeightedQuickUnion datastructure to maintain states of connected
+ *         sites. In Percolation, we using another grid to maintain sites
  */
 public class Percolation {
-	WeightedQuickUnionUF uf;
-	int columns;
-	int[] arr;
-	int count = 0;
-	int imaginarySite1;
-	int imaginarySite2;
-	
+	private WeightedQuickUnionUF uf;
+	private int columns;
+	private int[] arr;
+	private int imaginarySite1;
+	private int imaginarySite2;
+
 	/**
 	 * 
 	 * @param n
+	 *            create n-by-n grid, with all sites blocked adding extra two
+	 *            virtual sites
 	 */
-	public Percolation(int n) { // create n-by-n grid, with all sites blocked
-		uf = new WeightedQuickUnionUF(n * n + 2); //adding extra two virtual sites
-		arr = new int[n*n + 2];
+	public Percolation(int n) {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
+		uf = new WeightedQuickUnionUF(n * n + 2);
+	
+		arr = new int[n * n + 2];
 		columns = n;
-		imaginarySite1 = n*n ;
-		imaginarySite2 = n*n + 1;
-		//keep track of open sites with array
+		imaginarySite1 = n * n;
+		imaginarySite2 = n * n + 1;
+		// keep track of open sites with array
 		for (int i = 0; i < n * n + 2; i++) {
 			arr[i] = i;
-		}	
-		//set top and bottom rows of weightedQU array to imaginary sites
-		for (int i = 0; i < n; i++){
+		}
+		// set top and bottom rows of weightedQU array to imaginary sites
+		for (int i = 0; i < n; i++) {
 			uf.union(imaginarySite1, i);
-			uf.union(imaginarySite2, columns * columns - (n - i));
+			//uf.union(imaginarySite2, columns * columns - (n - i));
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param row
 	 * @param col
+	 * 
+	 *            open site (row, col) if it is not open already
 	 */
-	public void open(int row, int col) { // open site (row, col) if it is not								// open already
-	    if (!isOpen(row, col)) {
-	    	int id = xyToID(row, col);
-	    	//open site by setting its value to something other than id
-	    	arr[id] = id + 1;
-	    	//check adjacent site to see if its open and connect
-	    	if ((col + 1 <= columns) && isOpen(row, col + 1)) {
-	    		int idRight = xyToID(row, col + 1);
-	    		uf.union(id, idRight);
-	    	}
-	    	if (col - 1 > 0  && isOpen(row, col - 1)) {
-	    		int idLeft = xyToID(row, col - 1);
-	    		uf.union(id, idLeft);
-	    	}
-	    	if (row + 1 <= columns && isOpen(row + 1, col)) {
-	    		int idUp = xyToID(row + 1, col);
-	    		uf.union(idUp, id);
-	    	}
-	    	if (row - 1 > 0 && isOpen(row - 1, col)) {
-	    		int idDown = xyToID(row - 1, col);
-	    		uf.union(idDown, id);
-	    	}
-	    }
-	   
+	public void open(int row, int col) {
+		validate(row, col);
+		if (!isOpen(row, col)) {
+			int id = xyToID(row, col);
+			// open site by setting its value to something other than id
+			arr[id] = id + 1;
+			// check adjacent site to see if its open and connect
+			if ((col + 1 <= columns) && isOpen(row, col + 1)) {
+				int idRight = xyToID(row, col + 1);
+				uf.union(id, idRight);
+			}
+			if (col - 1 > 0 && isOpen(row, col - 1)) {
+				int idLeft = xyToID(row, col - 1);
+				uf.union(id, idLeft);
+			}
+			if (row + 1 <= columns && isOpen(row + 1, col)) {
+				int idUp = xyToID(row + 1, col);
+				uf.union(idUp, id);
+			}
+			if (row - 1 > 0 && isOpen(row - 1, col)) {
+				int idDown = xyToID(row - 1, col);
+				uf.union(idDown, id);
+			}
+		}
+		connectToBottomImaginarySite(row, col);
 	}
-	
+
 	/**
-	 * 
 	 * @param row
 	 * @param col
-	 * @return
+	 * @return true is site (row, col) open?
 	 */
-	public boolean isOpen(int row, int col) { // is site (row, col) open?
+	public boolean isOpen(int row, int col) {
+		validate(row, col);
 		int id = xyToID(row, col);
 		return arr[id] != id;
-
 	}
-	
+
 	/**
-	 * 
 	 * @param row
 	 * @param col
-	 * @return
+	 * 
+	 * @return public true if site is full
 	 */
-	public boolean isFull(int row, int col) { // is site (row, col) full?
+	public boolean isFull(int row, int col) {
+		validate(row, col);
 		int id = xyToID(row, col);
 		if (isOpen(row, col)) {
 			return uf.connected(id, imaginarySite1);
 		}
 		return false;
 	}
-	
+
 	/**
-	 * 
 	 * @return
 	 */
-	public int numberOfOpenSites() { // number of open sites
-		int count = 0; 
+	public int numberOfOpenSites() {
+		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] != i) {
 				count++;
@@ -113,32 +114,46 @@ public class Percolation {
 		}
 		return count;
 	}
-	
+
 	/**
-	 * 
 	 * @return true if the system percolates and false if it does not
 	 */
-	public boolean percolates() { // does the system percolate?
+	public boolean percolates() {
+		if (columns == 1) {
+			return isOpen(1, 1);
+		}
 		return uf.connected(imaginarySite1, imaginarySite2);
 	}
-	
+
 	/**
-	 * 
 	 * @param row
 	 * @param col
-	 * @return the mapped 1 dimensional ID of a 2 dimensional grid 
+	 * 
+	 * @return the mapped 1 dimensional ID of a 2 dimensional grid
 	 */
 	private int xyToID(int row, int col) {
 		return (row - 1) * columns + (col - 1);
 	}
+
+	private void validate(int row, int col) {
+		int id = xyToID(row, col);
+		if (row > columns || col > columns || row < 1 || col < 1) {
+			throw new IndexOutOfBoundsException("row " + row + " or column " + col + " is not between  and " + columns);
+		}
+	}
 	
+	private void connectToBottomImaginarySite(int row, int col) {
+		
+		if (row == columns && isOpen(row, col)) {
+			int id = xyToID(row, col);
+			uf.union(imaginarySite2, id);
+		}
+	}
+
 	/**
-	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) { // test client (optional)
-		int n = StdIn.readInt();
-		
+	public static void main(String[] args) {
 		
 	}
 
--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -2,71 +2,96 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
+/**
+ * 
+ * @author konadip
+ * 
+ */
 public class PercolationStats {
 	private double[] means;
-	private int T;
-	
+	private int trials;
+	private int size;
+
 	/**
 	 * 
-	 * @param n is used to create an n by n grid
-	 * @param trials is the number of independent experiments to be performed
-	 * @throws IllegalAccessException if {@code n <= 0} or {@code trials <= 0}
+	 * @param n
+	 *            is used to create an n by n grid
+	 * @param trials
+	 *            is the number of independent experiments to be performed
+	 * @throws IllegalAccessException
+	 *             if {@code n <= 0} or {@code trials <= 0} perform trials
+	 *             independent experiments on an n-by-n grid
 	 */
-	public PercolationStats(int n, int trials) throws IllegalAccessException {    // perform trials independent experiments on an n-by-n grid
-		if (n <= 0 || trials <= 0) {
-			throw new IllegalAccessException("Either n or trials is less than or equal to zero");
+	public PercolationStats(int n, int t) throws IllegalArgumentException {
+		if (n <= 0 || t <= 0) {
+			throw new IllegalArgumentException("Either n or trials is less than or equal to zero");
 		}
-		means = new double[trials];
-		T = trials;
-	}
-	
-	public double mean() {                          // sample mean of percolation threshold
-
-//		double sumOfMeans = 0;
-//		for (double d : means) {
-//			sumOfMeans += d;
-//		}
-//		return sumOfMeans/T;
+		means = new double[t];
+		trials = t;
+		size = n;
 		
-		return StdStats.mean(means);
-	}
-	public double stddev() {                       // sample standard deviation of percolation threshold
-		return StdStats.stddev(means);
-	}
-	
-	public double confidenceLo() {                 // low  endpoint of 95% confidence interval
-		double mean = mean();
-		double stddev = stddev();
-		double lo = mean - (1.96 * stddev)/Math.sqrt(T);
-		return lo;
-	}
-	
-	public double confidenceHi(){
-		double mean = mean();
-		double stddev = stddev();
-		double hi = mean + (1.96 * stddev)/Math.sqrt(T);
-		return hi;
-	}
-	
-	public static void main(String[] args) throws IllegalAccessException {
-		int n =  2 ;//StdIn.readInt();
-		int T = 1000000; //StdIn.readInt();
-		System.out.println("running");
-		PercolationStats ps = new PercolationStats(n, T);
-		for (int i = 0; i < T; i++) {
+		for (int i = 0; i < t; i++) {
 			Percolation p = new Percolation(n);
 			while (!p.percolates()) {
 				int row = StdRandom.uniform(1, n + 1);
 				int col = StdRandom.uniform(1, n + 1);
-				p.open(row, col);
+				if (!p.isOpen(row, col)) {
+					p.open(row, col);
+				}	
 			}
 			int openSites = p.numberOfOpenSites();
-			double mean = (double)openSites/(n * n);
-			ps.means[i] = mean;
+			double mean = (double) openSites / (n * n);
+			means[i] = mean;
 		}
-		
+	}
+
+	/**
+	 * 
+	 * @return sample mean of percolation threshold
+	 */
+	public double mean() {
+		return StdStats.mean(means);
+	}
+
+	/**
+	 * 
+	 * @return sample standard deviation of percolation threshold
+	 */
+	public double stddev() {
+		if (size == 1) return Double.NaN;
+		return StdStats.stddev(means);
+	}
+
+	/**
+	 * @return low endpoint of 95% confidence interval
+	 */
+	public double confidenceLo() {
+		double mean = mean();
+		double stddev = stddev();
+		return mean - (1.96 * stddev) / Math.sqrt(trials);
+	}
+
+	/**
+	 * @return high endpoint of 95% confidence interval
+	 */
+	public double confidenceHi() {
+		double mean = mean();
+		double stddev = stddev();
+		return mean + (1.96 * stddev) / Math.sqrt(trials);
+	}
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n = 2;// StdIn.readInt();
+		int t = 1000000; //StdIn.readInt();
+
+		PercolationStats ps = new PercolationStats(n, t);
+
 		System.out.println("mean = " + ps.mean());
 		System.out.println("stddev = " + ps.stddev());
-		System.out.println("95% confidence interval = [" + ps.confidenceLo() +"," + ps.confidenceHi() + "]" );
+		System.out.println("95% confidence interval = [" + ps.confidenceLo() + "," + ps.confidenceHi() + "]");
 	}
 }
